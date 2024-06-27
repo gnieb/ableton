3e75a56ed4370ba94e01136922e64717c8e8859c
Grace Nieboer <grace.nieboer@outlook.com>
2024-06-26 21:09:19 -0400

    play with landing page and some display elements

--- src/app/page.js
+++ src/app/page.js
@@ -20,8 +20,8 @@ export default function Home() {
               src="/vercel.svg"
               alt="Vercel Logo"
               className="dark:invert"
-              width={100}
-              height={24}
+              width={200}
+              height={50}
               priority
             />
           </a>
@@ -33,8 +33,8 @@ export default function Home() {
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/next.svg"
           alt="Next.js Logo"
-          width={180}
-          height={37}
+          width={360}
+          height={50}
           priority
         />
       </div>
